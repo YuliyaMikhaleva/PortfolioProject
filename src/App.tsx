8ccc0a213fbf142e8f69911462d8c668c6860802
YuliaMikhaleva <umihaleva93@mail.ru>
2024-07-23 13:29:41 +0500

    feat: added settings for projects, new dependencies and styles

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import 'assets/styles/style.scss'
+
+function App() {
+
+    return (
+        <>
+            <div className="content">
+                <div className="content__text">content</div>
+            </div>
+        </>
+    )
+}
+
+export default App
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,27 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      assets: "/src/assets",
+      components: "/src/components",
+      contexts: "/src/contexts",
+      helpers: "/src/helpers",
+      hooks: "/src/hooks",
+      layouts: "/src/layouts",
+      pages: "/src/pages",
+      store: "/src/store",
+      templates: "/src/templates",
+      icons: "/src/assets/icons",
+      mixins: "/src/assets/styles/_mixins.scss"
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@use "assets/styles/_mixins.scss" as *;'
+      }
+    }
+  },
   plugins: [react()],
 })
