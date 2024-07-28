bc12f22bc3aa84c8b6de3cac3fc3fe2c6d2eeca2
YuliaMikhaleva <umihaleva93@mail.ru>
2024-07-28 16:59:44 +0500

    feat: added new component Loader

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,14 @@
+import "./Loader.scss"
+
+const Loader = () => {
+
+    return (
+        <div className="loader">
+            <span className="loader__element"></span>
+            <span className="loader__element"></span>
+            <span className="loader__element"></span>
+        </div>
+    )
+}
+
+export default Loader
