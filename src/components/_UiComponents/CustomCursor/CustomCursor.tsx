6d1b4bf7748aec93fc162f64cf93b6e4187e0f14
YuliaMikhaleva <umihaleva93@mail.ru>
2024-07-28 16:43:40 +0500

    chore: changed CustomCursor

--- src/components/_UiComponents/CustomCursor/CustomCursor.tsx
+++ src/components/_UiComponents/CustomCursor/CustomCursor.tsx
@@ -3,16 +3,44 @@ import {useEffect, useRef} from "react";
 
 const CustomCursor = () => {
     const cursor = useRef<HTMLDivElement>(null);
+    const point = useRef<HTMLDivElement>(null);
     const moveCursor = (e) => {
         if (cursor && cursor.current) {
-            cursor.current.style.left = (e.clientX - 20) + 'px'
-            cursor.current.style.top = (e.clientY - 20 + window.scrollY) + 'px';
+            cursor.current.style.left = (e.clientX - 15) + 'px'
+            cursor.current.style.top = (e.clientY - 15) + 'px';
+        }
+        if (point && point.current) {
+            point.current.style.left = (e.clientX - 5) + 'px'
+            point.current.style.top = (e.clientY - 7) + 'px';
         }
     };
-    window.addEventListener('mousemove', moveCursor)
+    const hoverCursor = () => {
+        if (cursor && cursor.current) {
+            cursor.current.style.transform = "scale(4)"
+        }
+    };
+    const hoverOutCursor = () => {
+        if (cursor && cursor.current) {
+            cursor.current.style.removeProperty("transform");
+        }
+    };
+    useEffect(() => {
+        window.addEventListener('mousemove', moveCursor);
+        const links = document.body.querySelectorAll('a');
+        links.forEach(link => {
+            link.addEventListener('mouseenter', hoverCursor);
+            link.addEventListener('mouseleave', hoverOutCursor);
+        })
+    }, []);
+
+
 
     return (
-        <div className="custom-cursor" ref={cursor}></div>
+        <div className="custom-cursor">
+            <div className="custom-cursor__large" ref={cursor}></div>
+            <div className="custom-cursor__small" ref={point}></div>
+        </div>
+
     )
 }
 
