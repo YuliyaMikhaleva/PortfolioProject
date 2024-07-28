57975d5fe2987f7796e874b460d6f16f4481055a
YuliaMikhaleva <umihaleva93@mail.ru>
2024-07-28 16:42:50 +0500

    feat: added new component BubbleButton

---
+++ src/components/_UiComponents/BubbleButton/BubbleButton.tsx
@@ -0,0 +1,41 @@
+import "./BubbleButton.scss"
+import {memo, useRef} from "react";
+import * as React from "react";
+
+export interface IBubbleButtonProps {
+    children?: React.ReactNode;
+    className?: string;
+    white?:boolean
+
+}
+
+const BubbleButton: React.FC<IBubbleButtonProps> = memo(({children, className, white}) => {
+    const spanEl = useRef<HTMLSpanElement>()
+    const buttonEl = useRef<any>()
+    const handler = (e:any) => {
+        if (buttonEl && buttonEl.current && spanEl && spanEl.current) {
+            document.documentElement.style.setProperty('--button-width', `${buttonEl.current.getBoundingClientRect().width}px`);
+            var parentOffset = buttonEl.current.getBoundingClientRect();
+            var relX = e.clientX - parentOffset.left;
+            var relY = e.clientY - parentOffset.top;
+            spanEl.current.style.top = relY + 'px';
+            spanEl.current.style.left = relX + 'px';
+        }
+    }
+
+    const classes = [
+        'bubble-button',
+        className,
+        white && 'bubble-button--white'
+    ].join(' ');
+
+
+    return (
+        <button className={classes} ref={buttonEl} onMouseEnter={handler}>
+            {children}
+            <span ref={spanEl}></span>
+        </button>
+    )
+});
+
+export default BubbleButton
