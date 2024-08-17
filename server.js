2b1747548e63f70ea5bd6ebce4ddea752ad2f5dc
YuliaMikhaleva <umihaleva93@mail.ru>
2024-08-18 00:45:32 +0500

    test server.js

--- server.js
+++ server.js
@@ -10,14 +10,20 @@ app.use((req, res, next) => {
     next();
 })
 app.use(express.json())
-app.get('*', function (req, res) {
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 // Создание GET маршрута
 app.get('/express_backend', (req, res) => { //Строка 9
-    console.log('запрос')
     res.json({ data: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
     // res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Строка 10
 });
+const todoItems = require('./todo-items.json');
+app.get('/api/todo-items', (req, res) => {
+    res.json({ data: todoItems });
+})
 
-app.listen(3000);
+// app.listen(3000);
+app.listen(3000, () => {
+    console.log(`Server running on port ${3000}`);
+});
