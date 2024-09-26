e6ec0a76facac36ba6e8819a227fd2595a6732d6
YuliaMikhaleva <umihaleva93@mail.ru>
2024-09-26 14:55:38 +0500

    feat: added validation to TextInput

--- src/components/_UiComponents/TextInput/TextInput.tsx
+++ src/components/_UiComponents/TextInput/TextInput.tsx
@@ -1,5 +1,5 @@
 import "./TextInput.scss"
-import {memo} from "react";
+import {memo, useEffect, useState} from "react";
 import * as React from "react";
 
 export interface ITextInputProps {
@@ -7,22 +7,45 @@ export interface ITextInputProps {
     placeholder?: string;
     value: string;
     onChange: (e:any) => void;
-
+    name: 'email' | 'name';
 }
 
-const TextInput: React.FC<ITextInputProps> = memo(({className, placeholder, value='', onChange}) => {
+const TextInput: React.FC<ITextInputProps> = memo(({className, placeholder, value='', onChange, name}) => {
 
+    const [error, setError] = useState('');
+    const isValidEmail = (email: string) =>  {
+        const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        return emailRegex.test(email);
+    }
+    const isValidName = (name: string) =>  {
+        const nameRegex = /^[a-zA-Zа-яёА-ЯЁ ]*$/
+        return nameRegex.test(name);
+    }
     const classes = [
         'text-input',
         className,
-        value.length && 'text-input--active'
+        value.length && 'text-input--active',
+        error && 'text-input--error',
     ].join(' ');
 
+    useEffect(() => {
+        console.log('name', name)
+        console.log('!', name === 'email' && !isValidEmail(value))
+        if (value.length) {
+            (name === 'email' && !isValidEmail(value)) ? setError('Пример: ivanov@mail.ru') : (
+                (name === 'name' && !isValidName(value)) ? setError('В имени должны содержаться только буквы') : setError('')
+            );
+        } else {
+            setError('')
+        }
+
+    }, [value, name]);
 
     return (
         <div className={classes}>
-            <input type="text" value={value} onChange={onChange} onPaste={onChange} />
+            <input type="text" value={value} onChange={onChange} onPaste={onChange}/>
             <span className="text-input__placeholder">{placeholder}</span>
+            <span className="text-input__error">{error}</span>
         </div>
     )
 });
